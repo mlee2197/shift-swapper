54f30e25611397d5898835bcf21a307ad07fda33
Matt Lee <mlee138@binghamton.edu>
2019-04-15 01:53:10 +0000

    Additional changes to create function in session controller --Kamal

--- shift-swapper/app/controllers/sessions_controller.rb
+++ shift-swapper/app/controllers/sessions_controller.rb
@@ -16,8 +16,45 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render "shifts/dashboard"
+    auth_hash = request.env['omniauth.auth']
+    puts "\nauth_hash\n"
+    p auth_hash
+    puts ' '
+    auth_hash.each_pair do |key, value| 
+      puts "KEY: #{key}"
+      if value.kind_of?(Hash)
+        nested_hash(value)
+      else
+        puts "VALUE: #{value}"
+      end
+    end
+    #p auth_hash.kind_of?(Hash)
+    puts ' '
+    @authorization = Authorization.find_by(provider: auth_hash["provider"], uid: auth_hash["uid"])
+    puts "\nauthorization\n"
+    p @authorization
+    puts ' '
+    if @authorization
+      #render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+      render "shifts/dashboard"
+    else
+      @user = User.create! :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
+      @authorization = 
+        @user.authorizations.create :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+      puts "\n @user\n"
+      p @user
+      puts '\n'   
+      puts "\n @user.authorizations\n"
+      p @user.authorizations
+      puts '\n'        
+      #render :text => "Hi #{@user.name}! You've signed up."
+      render "shifts/dashboard"
+    end 
   end
+  
+  #def create
+  #  render "shifts/dashboard"
+  #end
 
   def failure
   end
